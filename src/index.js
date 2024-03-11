88b4adc6f4bc45b4975abd3846e90e0bdcb08401
Sameer <ccontact.sameer04@gmail.com>
2024-03-11 22:56:15 +0530

    add achievement section

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-const express = require("express");
+const express = require('express');
 const connect = require('./config/database');
 const { PORT } = require('./config/server-config');
 const apiRoutes = require('./routes/index');
 const cors = require('cors');
 
-
 const setupAndStartServer = () => {
-    const app = express();
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cors({
-        origin: 'https://sacc-minor-project.vercel.app',
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
-        credentials: true,
-      }));
-      
-    app.use('/api', apiRoutes);
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type'],
+      credentials: true,
+    })
+  );
 
-    app.listen(PORT, async () => {
-        console.log(`Server started at ${PORT}`);
-        await connect();
-        console.log('MongoDB connected');
-    });
-}
+  app.use('/api', apiRoutes);
+
+  app.listen(PORT, async () => {
+    console.log(`Server started at ${PORT}`);
+    await connect();
+    console.log('MongoDB connected');
+  });
+};
 
 setupAndStartServer();
